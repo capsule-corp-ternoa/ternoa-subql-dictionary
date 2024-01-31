0cd76bca86b6a3305d233bbd05108e2b633bc8e9
Victor-Salomon <victor-salomon@hotmail.fr>
2024-01-31 11:51:21 +0100

    fix blocks code after upgrade

--- src/handlers/block.ts
+++ src/handlers/block.ts
@@ -1,41 +1,41 @@
 import { SubstrateBlock } from "@subql/types";
-import { Block, Session, SpecVersion} from "../types";
+import { Block, Session, SpecVersion } from "../types";
 
 export const blockHandler = async (block: SubstrateBlock, specVersion: SpecVersion): Promise<void> => {
-    try{
+    try {
         const blockHeader = block.block.header
         const blockExtrinsics = block.block.extrinsics
         if (block.block.header.number.toNumber() % 100 === 0) logger.info(`Time ${blockHeader.number.toString()}: ${new Date()}`);
-        const blockRecord = new Block(blockHeader.number.toString())
-        blockRecord.number = blockHeader.number.toNumber()
-        blockRecord.hash = blockHeader.hash.toString()
-        blockRecord.timestamp = block.timestamp
-        blockRecord.parentHash = blockHeader.parentHash.toString()
-        blockRecord.stateRoot = blockHeader.stateRoot.toString()
-        blockRecord.extrinsicsRoot = blockHeader.extrinsicsRoot.toString()
-        blockRecord.nbExtrinsics = blockExtrinsics.length
-        blockRecord.runtimeVersion = block.specVersion
+        const number = blockHeader.number.toNumber()
+        const hash = blockHeader.hash.toString()
+        const timestamp = block.timestamp
+        const parentHash = blockHeader.parentHash.toString()
+        const stateRoot = blockHeader.stateRoot.toString()
+        const extrinsicsRoot = blockHeader.extrinsicsRoot.toString()
+        const runtimeVersion = block.specVersion
+        const nbExtrinsics = blockExtrinsics.length
+        const blockRecord = new Block(blockHeader.number.toString(), number, hash, timestamp, parentHash, stateRoot, extrinsicsRoot, runtimeVersion, nbExtrinsics)
         await updateSession(blockRecord)
-        await updateSpecversion(specVersion, block.specVersion, blockHeader.number.toBigInt())
+        await updateSpecversion(specVersion, runtimeVersion, blockHeader.number.toBigInt())
         await blockRecord.save()
-    }catch(err){
+    } catch (err) {
         logger.error('record block error:' + block.block.header.number.toNumber());
         logger.error('record block error detail:' + err);
     }
 }
 
 export const updateSession = async (blockRecord: Block) => {
-    try{
+    try {
         const sessionId = await api.query.session.currentIndex()
         let sessionRecord = await Session.get(sessionId.toString())
-        if (!sessionRecord){
-            sessionRecord = new Session(sessionId.toString())
+        if (!sessionRecord) {
             const validators = (await api.query.session.validators()) as unknown as string[]
-            sessionRecord.validators = validators.map(x => x.toString())
+            const formatedValidators = validators.map(x => x.toString())
+            sessionRecord = new Session(sessionId.toString(), formatedValidators)
             await sessionRecord.save()
         }
         blockRecord.sessionId = Number(sessionRecord.id)
-    }catch(err){
+    } catch (err) {
         logger.error('update session error');
         logger.error('update session error detail:' + err);
     }
@@ -45,9 +45,9 @@ export const updateSpecversion = async (specVersion: SpecVersion, blockSpecVersi
     if (!specVersion) {
         specVersion = await SpecVersion.get(blockSpecVersion.toString());
     }
-    if(!specVersion || specVersion.id !== blockSpecVersion.toString()){
-        specVersion = new SpecVersion(blockSpecVersion.toString());
-        specVersion.blockHeight = blockNumber;
+    if (!specVersion || specVersion.id !== blockSpecVersion.toString()) {
+        const blockHeight = blockNumber;
+        specVersion = new SpecVersion(blockSpecVersion.toString(), blockHeight);
         await specVersion.save();
     }
 }
